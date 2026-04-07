135e709925b965d3a4917a7cc9b222c420d8e90d
Артем <@DESKTOP-HI1680D>
2026-04-07 19:50:20 +0300

    Add project files.

---
+++ SalesManagerPro/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace SalesManagerPro
+{
+    public partial class MainWindow : Window
+    {
+        // Твій рядок підключення до бази
+        private string connString = @"Data Source=.\SQLEXPRESS;Initial Catalog=SalesDB;Integrated Security=True";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    // Запит до твоєї таблиці Clients
+                    SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Clients", conn);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    // Відправляємо дані в таблицю на екрані
+                    MyGrid.ItemsSource = table.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка: " + ex.Message);
+            }
+        }
+    }
+}
